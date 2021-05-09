52656db1f32f3f0d118992611112b4473a9e13d5
shrkdvy <shrkdvy123@gmail.com>
2021-05-09 17:41:20 +0900

    Delete Javascript directory

---
+++ Browser101/CSSOM.md
@@ -0,0 +1,9 @@
+# 1. CSSOM(Css Object Model) 
+- 브라우저에서 html파일을 돔트리로 변환후 css를 병합하여 구성.
+<img width="700" alt="CSSOM" src="https://user-images.githubusercontent.com/58588011/117447398-6af04680-af78-11eb-97ea-9cd797c7f258.png">
+
+- external : html태그에 정의한 css
+- embedded : css파일첨부 
+- inline : 브라우저자체 내장 css
+- cascading rules : 부모요소에 스타일을 적용시 그 자식 요소들은 자동으로 적용.
+<img width="700" alt="스크린샷 2021-05-07 오후 9 21 30" src="https://user-images.githubusercontent.com/58588011/117448841-32ea0300-af7a-11eb-85ac-6ac5c94dbc08.png">
---
+++ Browser101/Shopping_List/main.js
@@ -0,0 +1,39 @@
+const ItemList = document.querySelector('.items-list');
+const input = document.querySelector('.input');
+const plus = document.querySelector('.plus');
+
+
+let i = 0;
+input.addEventListener('change',(e) => {
+    const target = e.target;
+    let value = target.value;
+    
+    const itemContainer = document.createElement('div');
+    itemContainer.setAttribute('class', 'item-container');
+    itemContainer.innerHTML =`
+    <div class="item">
+        <span class="item-name">${value}</span>
+        <button class="tresh-icon" data-num=${i} >
+            <i class="fas fa-trash-alt"></i>
+        </button>
+    </div>
+    <div class="line"></div>
+    `
+    ItemList.appendChild(itemContainer);
+
+
+    const deleteIcon = document.querySelector(`[data-num = '${i}']`);
+    deleteIcon.addEventListener('click',(event) => {
+        let target = event.target;
+
+        if(target.nodeName === 'I') {
+            target = target.parentNode;
+        }
+
+        const item = target.parentNode;
+        const itemContainer = item.parentNode;
+        itemContainer.remove();
+    })
+    i++;
+    input.value = "";
+}) 
