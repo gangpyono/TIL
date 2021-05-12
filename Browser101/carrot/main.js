5038e7a5492b7f80ad5417de4d6c5d667cfe6fdc
shrkdvy <shrkdvy123@gmail.com>
2021-05-12 23:49:55 +0900

    update
    Audio,PopUp

--- Browser101/carrot/main.js
+++ Browser101/carrot/main.js
@@ -1,13 +1,19 @@
+const bgm = new Audio('sound/bg.mp3');
+const carrotSound = new Audio('sound/carrot_pull.mp3');
+const bugSound = new Audio('sound/bug_pull.mp3');
+const winSound = new Audio('sound/game_win.mp3');
+
 const gameField = document.querySelector('.game-field');
 const carrot = document.querySelector('.carrot');
 const bug = document.querySelector('.bug');
 
-
-
-
 const controlBtn = document.querySelector('.control__btn');
 const controlScore = document.querySelector('.control__score');
-const controlCounter = document.querySelector('.control__counter');
+const controlTimer = document.querySelector('.control__timer');
+
+const PopUp = document.querySelector('.Pop-up');
+const PopUpBtn = document.querySelector('.Pop-up__btn');
+const PopUpMessage = document.querySelector('.Pop-up__message');
 
 const gameFieldWidth = gameField.getBoundingClientRect().width;
 const gameFieldHeight = gameField.getBoundingClientRect().height;
@@ -15,14 +21,58 @@ const gameFieldHeight = gameField.getBoundingClientRect().height;
 let carrotNum = 20;
 const bugNum = 10;
 const carrotSize = 80; // 이미지 사이즈
-let time = 80; //sec
 
+let time = 20; //sec
+let timerId = '';
 
 let started = true;
+
+
 
 function createField () {
     createItem("carrot", carrotNum);
     createItem("bug", bugNum);
+    gameField.addEventListener('click',ClickEvnet);
+}
+
+function ClickEvnet (event) {
+    const target = event.target;
+    if(target.className === "carrot") {
+            carrotSound.play();
+            target.remove();
+            --carrotNum;
+            score(); 
+    }
+
+    if(target.className === "bug") {
+            bugSound.play();
+            PopUpMessage.textContent = " YOT LOST!";
+            gameover();   
+    }
+
+    if(carrotNum === 0) {
+            PopUpMessage.innerText = "YOU WON!";
+            winSound.play();
+            gameover();
+    }
+ }
+
+function createItem(item, item_num) {
+    for( let i = 0 ; i<item_num; i++) {
+        const toBeCreated = document.createElement('img');
+              toBeCreated.setAttribute('src',`img/${item}.png`);
+              toBeCreated.setAttribute('alt',`${item}`);
+              toBeCreated.setAttribute('class',`${item}`);
+
+        
+        const x = createPositionX();
+        const y = createPositionY();
+
+        toBeCreated.style.left = `${x}px`;
+        toBeCreated.style.top = `${y}px`;
+
+        gameField.appendChild(toBeCreated);
+  }
 }
 
 function createPositionX () {
@@ -35,73 +85,68 @@ function createPositionY () {
     return y;
 }
 
-function createItem(item, item_num) {
-    
-    for( let i = 0 ; i<item_num; i++) {
-        const toBeCreated = document.createElement('img');
-        toBeCreated.setAttribute('src',`img/${item}.png`);
-        toBeCreated.setAttribute('alt',`${item}`);
-        toBeCreated.setAttribute('class',`${item}`);
-        
-        const x = createPositionX();
-        const y = createPositionY();
+function timer () {
+    if(time === 0 ) {
+        PopUpMessage.textContent = " YOU LOST!";
+        gameover();
+    }
 
-        toBeCreated.style.left = `${x}px`;
-        toBeCreated.style.top = `${y}px`;
-
-            if(item === "carrot") {
-                toBeCreated.addEventListener( 'click',() => {
-                toBeCreated.remove();
-                --carrotNum;
-                score();
-          })
-        }else {
-             toBeCreated.addEventListener('click',() => {
-                 gameEnd();
-                 carrotNum = 20;
-             })
-        }
-    gameField.appendChild(toBeCreated);
-  }
-}
-
-
-
-
-
-function startGame () {
-    if(started) {
-    let timer = setInterval(counter, 1000);
-    createField();
-    counter();
-    score();
-    // carrotNum();
-    started = false;
-    controlBtn.innerHTML = `<i class="fas fa-pause pauseBtn"></i>`;
-
-    }    
-}
-
-function gameEnd () {
-    
-}
-
-
-
-function counter () {
-    let  minute = Math.floor(time / 60);
+    let minute = Math.floor(time / 60);
     let sec = time % 60;
-    controlCounter.innerText = `${minute}:${sec}`;    
+
+    controlTimer.innerText = `${minute}:${sec}`;    
     time--;
 }
-
-
 
 function score () {
     controlScore.innerText = carrotNum;
 }
 
+
+function startGame () {
+    bgm.play();
+    controlBtn.innerHTML = `<i class="fas fa-pause"></i>`;
+    HidePopup();
+    time = 20;
+    started = false;
+    createField();
+    timer();
+    timerId = setInterval(timer, 1000);
+    score();
+}
+
+function gameover () {
+    bgm.pause();
+    clearInterval(timerId);
+    popUp();
+    gameField.removeEventListener('click',ClickEvnet);
+    started = true;
+}
+
+function popUp () {
+    PopUp.style.visibility = 'visible';
+    controlBtn.style.visibility = "hidden"; 
+}
+
+function HidePopup () {
+    PopUp.style.visibility = "hidden";
+    controlBtn.style.visibility = "visible"; 
+}
+
+
+
 controlBtn.addEventListener('click', () => {
+  if(started) {
+    startGame();
+  } else {
+    PopUpMessage.textContent = "Replay?";
+    gameover();
+  }
+})  
+
+PopUpBtn.addEventListener('click', () => {
+    gameField.innerHTML = '';
+    carrotNum = 20;
     startGame();
 })
 
