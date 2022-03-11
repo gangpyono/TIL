e9137384dd339ad51c92d1a1bb529a44a0c532fa
shrkdvy <shrkdvy123@gmail.com>
2022-03-12 00:52:54 +0900

    ts union

--- typescript/1-TYPES/1-5-union.ts
+++ typescript/1-TYPES/1-5-union.ts
@@ -1,14 +1,15 @@
 {
-  // Union Types : OR로 이해하자. (ts에서의 활용도가 매우 높은 type)
+  // Union Types : OR(ts에서의 활용도가 매우 높은 type)
+  // 가능한 경우의수 중에서 하나의 타입을 선택하는 방식.
+  type Direction = "left" | "right" | "up" | "down"; // Union types
 
-  type Direction = "left" | "right" | "up" | "down";
-  // 가능한 경우의수 중에서 하나의 값을 선택하는 방식으로 작성하고 싶을떄.
   function move(direction: Direction) {
     console.log(direction);
   }
+
   move("down");
 
-  type TileSize = 8 | 16 | 32;
+  type TileSize = 8 | 16 | 32; // Union types
   const tile: TileSize = 8;
 
   //  function : login => success , fail
@@ -22,7 +23,7 @@
     reason: string;
   };
 
-  type LoginState = SuccessState | FailState;
+  type LoginState = SuccessState | FailState; // Union types
 
   function login(): LoginState {
     return {
@@ -31,9 +32,7 @@
       },
     };
   }
-  // union : 발생할 수 있는 다양한 case중 하나의 틀을 정하고 싶을떄.
 
-  //
   function printLoginState(state: LoginState) {
     if ("response" in state) {
       console.log(`🎉 ${state.response.body}`);
