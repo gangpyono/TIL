290db6d53cfcd0a204c3187ed85e9cb9e037e943
gangpyono <shrkdvy123@gmail.com>
2024-12-19 17:07:37 +0900

    feat: 추가 1번문제풀이

--- algorithm/leetcode/top_150/Hashmap/1. Two Sum.py
+++ algorithm/leetcode/top_150/Hashmap/1. Two Sum.py
@@ -2,28 +2,47 @@ from typing import List
 
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    # for loop
+    def twoSum1(self, nums: List[int], target: int) -> List[int]:
 
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
 
+    # dict
+    def twoSum2(self, nums: List[int], target: int) -> List[int]:
 
-print(Solution().twoSum([3, 3], 6))
-
-
-# 다시볼것
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-
-        numMap = {}
+        d = {}
 
         for i in range(len(nums)):
-            if target - nums[i] in numMap:
-                return [numMap[target - nums[i]], i]
+            if target - nums[i] in d:
+                return [d[target - nums[i]], i]
             else:
-                numMap[nums[i]] = i
+                d[nums[i]] = i
+
+    # backtracking
+    def twoSum3(self, nums: List[int], target: int) -> List[int]:
+
+        result = []
+
+        def dfs(i, total, indices):
+            if len(indices) == 2 and total == target:
+                result.extend(indices)
+                return
+
+            if len(indices) == 2:
+                return
+
+            for j in range(i, len(nums)):
+                indices.append(j)
+                dfs(j + 1, total + nums[j], indices)
+                indices.pop()
+
+        dfs(0, 0, [])
+        return result
 
 
-print(Solution().twoSum([2, 7, 11, 15], 9))
+print(Solution().twoSum1([3, 2, 4], 6))
+print(Solution().twoSum2([3, 2, 4], 6))
+print(Solution().twoSum3([3, 2, 4], 6))
