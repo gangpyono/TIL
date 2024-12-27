12890baad0c0b7c1a145ddde528eacd7095d86c2
gangpyono <shrkdvy123@gmail.com>
2024-12-27 18:55:13 +0900

    feat: 추가 433번문제풀이

---
+++ algorithm/leetcode/top_150/Graph BFS/433. Minimum Genetic Mutation.py
@@ -0,0 +1,26 @@
+from typing import List
+from collections import deque
+
+
+class Solution:
+    def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
+        q = deque([[startGene, 0]])
+        visit = set()
+        visit.add(startGene)
+
+        while q:
+            gen, cnt = q.popleft()
+            if gen == endGene:
+                return cnt
+
+            for i in range(len(gen)):
+                for char in "ACGT":
+                    mutatedGen = gen[:i] + char + gen[i + 1 :]
+                    if mutatedGen in bank:
+                        q.append([mutatedGen, cnt + 1])
+                        visit.add(mutatedGen)
+
+        return -1
+
+
+print(Solution().minMutation("AACCGGTT", "AACCGGTG", ["AACCGGTG"]))
