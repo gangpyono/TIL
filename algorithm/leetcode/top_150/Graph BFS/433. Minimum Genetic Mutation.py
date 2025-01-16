c05e8e18d24cb2a355e579a1203e5e0cfa9cf646
gangpyono <shrkdvy123@gmail.com>
2025-01-16 13:04:12 +0900

    feat: 추가 433번 문제풀이

--- algorithm/leetcode/top_150/Graph BFS/433. Minimum Genetic Mutation.py
+++ algorithm/leetcode/top_150/Graph BFS/433. Minimum Genetic Mutation.py
@@ -17,9 +17,32 @@ class Solution:
             for i in range(len(gen)):
                 for char in "ACGT":
                     mutatedGen = gen[:i] + char + gen[i + 1 :]
-                    if mutatedGen in bank:
+                    if mutatedGen in bank not in visit:
                         q.append([mutatedGen, cnt + 1])
                         visit.add(mutatedGen)
+
+        return -1
+
+    def minMutation1(self, startGene: str, endGene: str, bank: List[str]) -> int:
+        genChars = ["A", "C", "G", "T"]
+        q = deque()
+        visited = set()
+        visited.add(startGene)
+        q.append((startGene, 0))
+
+        while q:
+            gene, count = q.popleft()
+            if gene == endGene:
+                return count
+
+            for i in range(8):
+                for genChar in genChars:
+                    if gene[i] == genChar:
+                        continue
+                    mutatedGene = gene[:i] + genChar + gene[i + 1 :]
+                    if mutatedGene in bank and mutatedGene not in visited:
+                        visited.add(mutatedGene)
+                        q.append((mutatedGene, count + 1))
 
         return -1
 
