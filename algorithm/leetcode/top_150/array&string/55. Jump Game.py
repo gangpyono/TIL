d22be07ebf8d5d4fd99406bfabc65be958ab1094
gangpyono <shrkdvy123@gmail.com>
2025-02-16 19:19:17 +0900

    feat: 추가 55번 문제풀이

--- algorithm/leetcode/top_150/array&string/55. Jump Game.py
+++ algorithm/leetcode/top_150/array&string/55. Jump Game.py
@@ -1,14 +1,16 @@
 from typing import List
 
+
 class Solution:
-     def canJump(self, nums: List[int]) -> bool:
+    def canJump(self, nums: List[int]) -> bool:
 
         n = len(nums)
-        goal = n -1
+        goal = n - 1
 
-        for i in range(n-2, -1,-1):
+        for i in range(n - 1, -1, -1):
             if i + nums[i] >= goal:
                 goal = i
-        return True if goal == 0 else False
+        return goal == 0
 
-print(Solution().canJump([2,3,1,0,4]))
+
+print(Solution().canJump([2, 3, 1, 0, 4]))
